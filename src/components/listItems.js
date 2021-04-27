08265e454322885d5e421ed1891bc3ffe93f6070
Saifoo Khan <isaifookhan@gmail.com>
2021-04-27 14:05:33 +0200

    cleanup

--- src/components/listItems.js
+++ src/components/listItems.js
@@ -2,20 +2,11 @@ import React from "react";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import ListSubheader from "@material-ui/core/ListSubheader";
 import DashboardIcon from "@material-ui/icons/Dashboard";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
-import PeopleIcon from "@material-ui/icons/People";
-import BarChartIcon from "@material-ui/icons/BarChart";
 import LayersIcon from "@material-ui/icons/Layers";
 import AssignmentIcon from "@material-ui/icons/Assignment";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link as RLink,
-} from "react-router-dom";
+import { Link as RLink } from "react-router-dom";
 
 export const mainListItems = (
   <div>
@@ -44,18 +35,5 @@ export const mainListItems = (
         <ListItemText primary="Map View" />
       </ListItem>
     </RLink>
-
-    <ListItem button>
-      <ListItemIcon>
-        <BarChartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Reports" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <LayersIcon />
-      </ListItemIcon>
-      <ListItemText primary="Integrations" />
-    </ListItem>
   </div>
 );
