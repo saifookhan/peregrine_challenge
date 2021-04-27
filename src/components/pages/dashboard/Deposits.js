7f29527cad00fd29491ca54ee593b190aa2848d9
Saifoo Khan <isaifookhan@gmail.com>
2021-04-27 14:53:40 +0200

    dashboard complete

--- src/components/pages/dashboard/Deposits.js
+++ src/components/pages/dashboard/Deposits.js
@@ -1,35 +1,130 @@
-import React from 'react';
-import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Title from './Title';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import clsx from "clsx";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
 
-function preventDefault(event) {
-  event.preventDefault();
-}
+import Title from "./Title";
 
-const useStyles = makeStyles({
+import logsData from "../../../constants/events.json";
+
+const useStyles = makeStyles((theme) => ({
   depositContext: {
     flex: 1,
   },
-});
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column",
+  },
+  fixedHeight: {
+    maxHeight: 240,
+    maxWidth: 300,
+  },
+  backgroundBlue: {
+    background: "#e8e7ff",
+  },
+  backgroundRed: {
+    background: "#ffe7e7",
+  },
+  backgroundGrey: {
+    background: "#ededed",
+  },
+}));
 
 export default function Deposits() {
   const classes = useStyles();
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
   return (
     <React.Fragment>
-      <Title>Recent Deposits</Title>
-      <Typography component="p" variant="h4">
-        $3,024.00
-      </Typography>
-      <Typography color="textSecondary" className={classes.depositContext}>
-        on 15 March, 2019
-      </Typography>
-      <div>
-        <Link color="primary" href="#" onClick={preventDefault}>
-          View balance
-        </Link>
-      </div>
+      <Title>Total Collission Events</Title>
+      <Container maxWidth="lg" className={classes.container}>
+        <Grid container spacing={3}>
+          <Grid item>
+            <Paper className={clsx(fixedHeightPaper, classes.backgroundBlue)}>
+              <Typography component="p" variant="h6">
+                Acceleration
+              </Typography>
+              <Typography component="p" variant="h4">
+                {
+                  logsData.filter((r) => {
+                    return r.eventType === "breaking";
+                  }).length
+                }
+              </Typography>
+              <Typography
+                color="textSecondary"
+                className={classes.depositContext}
+              >
+                Percentage:{" "}
+                {(logsData.filter((r) => {
+                  return r.eventType === "breaking";
+                }).length /
+                  logsData.length) *
+                  100}
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item>
+            <Paper className={clsx(fixedHeightPaper, classes.backgroundRed)}>
+              <Typography component="p" variant="h6">
+                Collision
+              </Typography>
+              <Typography component="p" variant="h4">
+                {
+                  logsData.filter((r) => {
+                    return r.eventType === "collision";
+                  }).length
+                }
+              </Typography>
+              <Typography
+                color="textSecondary"
+                className={classes.depositContext}
+              >
+                Percentage:{" "}
+                {(logsData.filter((r) => {
+                  return r.eventType === "collision";
+                }).length /
+                  logsData.length) *
+                  100}
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item>
+            <Paper className={clsx(fixedHeightPaper, classes.backgroundGrey)}>
+              <Typography component="p" variant="h6">
+                Breaking
+              </Typography>
+              <Typography component="p" variant="h4">
+                {
+                  logsData.filter((r) => {
+                    return r.eventType === "breaking";
+                  }).length
+                }
+              </Typography>
+              <Typography
+                color="textSecondary"
+                className={classes.depositContext}
+              >
+                Percentage:{" "}
+                {(logsData.filter((r) => {
+                  return r.eventType === "breaking";
+                }).length /
+                  logsData.length) *
+                  100}
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </React.Fragment>
   );
 }
--- src/components/pages/dashboard/dashboard.js
+++ src/components/pages/dashboard/dashboard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Chart from "./Chart";
 import Deposits from "./Deposits";
-import Orders from "./Orders";
 
 import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
@@ -44,23 +43,17 @@ function Dashboard(props) {
         <Container maxWidth="lg" className={classes.container}>
           <Grid container spacing={3}>
             {/* Chart */}
+            <Grid item xs={12} md={12} lg={12}>
+              <Paper className={fixedHeightPaper}>
+                <Deposits />
+              </Paper>
+            </Grid>
             <Grid item xs={12} md={8} lg={9}>
               <Paper className={fixedHeightPaper}>
                 <Chart />
               </Paper>
             </Grid>
             {/* Recent Deposits */}
-            <Grid item xs={12} md={4} lg={3}>
-              <Paper className={fixedHeightPaper}>
-                <Deposits />
-              </Paper>
-            </Grid>
-            {/* Recent Orders */}
-            <Grid item xs={12}>
-              <Paper className={classes.paper}>
-                <Orders />
-              </Paper>
-            </Grid>
           </Grid>
         </Container>
         <Copyright />
